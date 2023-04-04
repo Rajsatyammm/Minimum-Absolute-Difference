eb339656c48d5039cbe943e2ed2f304f394a6032
Satyam Raj <rajsatyammm@gmail.com>
2023-04-04 21:57:54 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class minAbsDiffer {
+
+    public static int absoluteDifference(int[] A, int[] B) {
+        int diff = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=0; i<A.length; i++) {
+            int val = A[i] - B[i];
+            Math.abs(val);
+            diff += val; 
+        }
+        return diff;
+    }
+    public static void main(String[] args) {
+        // int[] A = {4, 1, 8, 7};
+        // int[] B = {2, 3, 6, 5};
+        int[] A = {1, 2, 3};
+        int[] B = {2, 1, 3};
+        
+        System.out.println(absoluteDifference(A, B));
+    }
+}
